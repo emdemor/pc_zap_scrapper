05ef0cc5e6fa0575093283480d17b1c0e3aa3cd8
Eduardo Morais <emdemor415@gmail.com>
2023-01-08 00:18:51 -0300

    Bump version: 0.1.0.dev0 → 0.1.0.dev1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ project = 'Poços de Caldas - Scrapper de Imóveis' # Change app_name
 copyright = '2023, Eduardo Morais'
 author = 'Eduardo Morais'
 release = '2023'
-version = '0.1.0.dev0'
+version = '0.1.0.dev1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
